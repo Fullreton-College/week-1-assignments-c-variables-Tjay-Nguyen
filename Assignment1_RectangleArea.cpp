96768b6941d141954ec402dcca9abf91ca82b4dc
Tjay-Nguyen <TjayN704@gmail.com>
2026-02-25 11:03:20 -0800

    Add area calculation and output for rectangle

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+	
 	// print name of program
 	cout << "Rectangle Calculator" << endl << endl;
 
@@ -13,12 +14,14 @@ int main()
 	cout << "Enter height and width: ";
 /* write your code here */
 	// calculate area
-	
+	cin >> height >> width;
+	double area = height * width;
 
 	// write output to console
-	
+	cout << "The area is: " << area << endl;
 
 	// return value that indicates normal program exit
+	// done with this
     return 0;
 }
 
